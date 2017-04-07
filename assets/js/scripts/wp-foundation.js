f3796bd3269a4e08d32f0536a2b784a481e1a4d9
ksascomm <ksasweb@jhu.edu>
2017-04-07 11:25:59 -0400

    use fancybox3 instead of lightbox2

--- assets/js/scripts/wp-foundation.js
+++ assets/js/scripts/wp-foundation.js
@@ -20,4 +20,8 @@ jQuery(document).ready(function() {
     }
   });
 
+
+ jQuery('a[href*=".jpg"], a[href*=".jpeg"], a[href*=".png"], a[href*=".gif"]').fancybox(); 
+ jQuery('.gallery a').attr("data-fancybox", "group");
+
 });
