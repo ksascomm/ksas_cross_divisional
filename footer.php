a393791e025bbf1980a601c1e735e6691e836fc0
ksascomm <ksasweb@jhu.edu>
2017-04-12 13:56:51 -0400

    remove 3 shields for hopkins-review footer

--- footer.php
+++ footer.php
@@ -6,26 +6,10 @@
   									<p>&copy; <?php print date('Y'); ?> Johns Hopkins University, <?php echo $theme_option['flagship_sub_copyright'];?></p>
 					  			</div>
 					  		</div>
-					  		<div class="row">
-						  		<?php global $blog_id; if ($blog_id == 93) : ?>
-	   
-								  		<div class="small-12 medium-4 columns">
-							  				<a href="http://www.jhu.edu" target="_blank"><img src="<?php echo get_template_directory_uri() ?>/assets/images/university.jpg" alt="Johns Hopkins University"/></a>
-							  			</div>
-							  			<div class="small-12 medium-4 columns">
-							  				<a href="http://www.press.jhu.edu" target="_blank"><img src="<?php echo get_template_directory_uri() ?>/assets/images/press.png" alt="University Press"/></a>
-							  			</div>
-							  			<div class="small-12 medium-4 columns">
-							  				<a href="http://www.krieger.jhu.edu" target="_blank"><img src="<?php echo get_template_directory_uri() ?>/assets/images/krieger.png" alt="Krieger School"/></a>
-							  			</div>
-
-									<?php  else : ?>
-		   
-								  		<div class="small-12 small-centered medium-4 columns">
-							  				<a href="http://www.jhu.edu" target="_blank"><img src="<?php echo get_template_directory_uri() ?>/assets/images/university.jpg" alt="Johns Hopkins University"/></a>
-							  			</div>
-							  			
-								<?php endif; ?>
+					  		<div class="row">		   
+						  		<div class="small-12 small-centered medium-4 columns">
+					  				<a href="http://www.jhu.edu" target="_blank"><img src="<?php echo get_template_directory_uri() ?>/assets/images/university.jpg" alt="Johns Hopkins University"/></a>
+					  			</div>	
 					  		</div>
 						</div> <!-- end #inner-footer -->
 					</footer> <!-- end .footer -->
