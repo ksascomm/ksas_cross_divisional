6267373e01ef8d54b03998d7061efc0d3d6092d2
ksascomm <ksasweb@jhu.edu>
2018-07-17 14:49:49 -0400

    remove column push

--- template-full-width.php
+++ template-full-width.php
@@ -10,7 +10,7 @@ Template Name: Full Width (No Sidebar)
 	
 		<div id="inner-content" class="row">
 	
-		    <main id="main" class="small-12 large-9 large-push-1 columns" role="main">
+		    <main id="main" class="small-12 large-9 columns" role="main">
 
 			<?php if (function_exists('dimox_breadcrumbs') ) { dimox_breadcrumbs();} ?>
 
