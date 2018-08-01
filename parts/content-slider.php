8ee7f88b3dc45a56442cc2520d99be82192e6585
ksascomm <ksasweb@jhu.edu>
2018-08-01 11:00:23 -0400

    use fontawesome arrow

--- parts/content-slider.php
+++ parts/content-slider.php
@@ -10,7 +10,7 @@
 			<?php endif;?>
 				<h5><?php echo get_the_content(); ?></h5>
 			   	<?php if ( get_post_meta($post->ID, 'ecpt_button', true) ) : ?>				
-						<a class="button" href="<?php echo get_post_meta($post->ID, 'ecpt_urldestination', true); ?>" onclick="ga('send', 'event', 'Homepage Slider', 'Click', '<?php echo get_post_meta($post->ID, 'ecpt_urldestination', true); ?>')" aria-label="<?php the_title(); ?>">Find Out More <span class="icon-arrow-right-2"></span></a> 
+						<a class="button" href="<?php echo get_post_meta($post->ID, 'ecpt_urldestination', true); ?>" onclick="ga('send', 'event', 'Homepage Slider', 'Click', '<?php echo get_post_meta($post->ID, 'ecpt_urldestination', true); ?>')" aria-label="<?php the_title(); ?>">Find Out More <span class="fa fa-arrow-circle-o-right" aria-hidden="true"></span></a> 
 				<?php endif;?>
 			</div>
 		</div>	
